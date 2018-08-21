a2ba33a617816a0c1c2ad15a0da70fe739bae742
alumno <alumno@LAB00PC00>
2018-08-21 20:57:30 -0300

    guardado

--- clase1/primerProyecto/Clase_1/main.c
+++ clase1/primerProyecto/Clase_1/main.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 int main()
 {
-    printf("Hello world!\n");
+    int a;
+    int b;
+    int c;
+    printf("ingrese el primer numero: ");
+    scanf("%i",&a);
+    printf("ingrese el segundo numero: ");
+    scanf("%i",&b);
+    printf("ingrese el tercer numero: ");
+    scanf("%i",&c);
+    if(a>b && a<c){
+        printf("el %i es el numero del medio.",a);
+    }else if(b>a && b<c){
+        printf("el %i es el numero del medio.",b);
+    }else{
+        printf("el %i es el numero del medio.",c);
+    }
     return 0;
 }
