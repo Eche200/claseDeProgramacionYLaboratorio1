50cfa53e2422e35428adf89997520376917f6320
alumno <alumno@LAB00PC00>
2018-08-23 21:07:50 -0300

    guardar

--- clase2/ejercicioClase2/EjercicioClase2Promedio/main.c
+++ clase2/ejercicioClase2/EjercicioClase2Promedio/main.c
@@ -3,12 +3,12 @@
 #define TAM 5
 int main()
 {
-    int nota[TAM];
+    float nota[TAM];
     int  acumulador=0;
     float promedio;
-    for(int i=0;i<5;i++){
+    for(int i=0;i<TAM;i++){
         printf("ingrese la nota numero %d: ",i+1);
-        while(scanf("%d",&nota[i])!=1 || (nota[i]<1 || nota[i]>10)){
+        while(scanf("%f",&nota[i])!=1 || (nota[i]<1 || nota[i]>10)){
             printf("Error , ingrese  otra vez la  nota");
             __fpurge(stdin);
         }
