a78a75ecdace074730c3bfadac3b3d1e33f2693b
alumno <alumno@LAB00PC00>
2018-08-30 19:49:55 -0300

    clase4

--- clase4/clase4Carpeta/Clase4/main.c
+++ clase4/clase4Carpeta/Clase4/main.c
@@ -1,8 +1,43 @@
-#include <stdio.h>
+#include <stdio_ext.h>
 #include <stdlib.h>
+int getNumero(int* pResultado,char* mensaje,char* mensajeError,int minimo,int maximo,int reintentos);
 
 int main()
 {
-    printf("Hello world!\n");
+    int numero;
+    if(getNumero(&numero,"ingrese un numero","Error  , ingrese un numero correcto",0,100,3)==0){
+        printf("el numero es %d",numero);
+    }
     return 0;
+}
+// char* mensaje  es igual a decirle que  va a recibir un STRING
+int getNumero(int* pResultado,char* mensaje,char* mensajeError,int minimo,int maximo,int reintentos){
+    int retorno=0;
+    int auxNumero;
+    do{
+        __fpurge(stdin);
+        printf("%s \n",mensaje);
+        if(scanf("%d",&auxNumero)!=1 || *pResultado<minimo || *pResultado>maximo ){
+            system("clear");
+            printf("%s\n",mensajeError);
+            reintentos--;
+        }else{
+            *pResultado=auxNumero;
+            break;
+        }
+        if(reintentos==0){
+            system("clear");
+            printf("Pasaste el limite de intentos, ADIOS.");
+            retorno=1;
+        }
+    }while(reintentos>0);
+    return retorno;
+}
+
+int validarInt(int auxNumero,int min , int max){
+
+
+
+
+
 }
