0de9ca67700b21515c533297c13097107d29772e
alumno <alumno@LAB00PC00>
2018-09-04 21:35:56 -0300

    clase 5

--- clase5/clase 5/clase5/main.c
+++ clase5/clase 5/clase5/main.c
@@ -1,71 +1,98 @@
 #include <stdio_ext.h>
 #include <stdlib.h>
 #define CANTIDAD 5
-int utn_getNumero(int* pResultado,char* mensaje,char* mensajeError,int minimo,int maximo,int reintentos);
-int utn_validarInt(int auxNumero,int min , int max);
+
+int retornarTotal(int listadoDeNotas[],int tamano);
+int retornarPromedio(int listadoDeNotas[],int tamano);
+void cargarListado(int listadoDeNotas[],int tamano);
+int numeroMayor(int listadoDeNotas[], int tamano);
+int numeroMenor(int listadoDeNotas[], int tamano);
+int cantidadDePares(int listadoDeNotas[], int tamano);
+void mostrarInformacionDeLaArray(int listadoDeNotas[],int tamano);
+
 int main()
 {
     int listadoDeNotas[CANTIDAD];
+    cargarListado(listadoDeNotas , CANTIDAD);
+    mostrarInformacionDeLaArray(listadoDeNotas,CANTIDAD);
+
+    return 0;
+}
+
+void mostrarInformacionDeLaArray(int listadoDeNotas[], int tamano){
+
+
+    printf("\nEl numero mayor es %d",numeroMayor(listadoDeNotas,tamano));
+    printf("\nEl numero menor es %d",numeroMenor(listadoDeNotas,tamano));
+    printf("\nLa cantidad de pares es de %d", cantidadDePares(listadoDeNotas,tamano));
+    printf("\nLa suma de todas las notas es %d", retornarTotal(listadoDeNotas,tamano));
+    printf("\nEl promedio es %d", retornarPromedio(listadoDeNotas,tamano));
+
+    //mostrar todo . mayor menor cantidad de pares, suma , promedio.
+
+}
+int cantidadDePares(int listadoDeNotas[], int tamano){
+
+    int contadorDePares=0;
+    for(int i=0;i<tamano;i++){
+
+        if(listadoDeNotas[i]%2==0){
+            contadorDePares++;
+        }
+    }
+    return contadorDePares;
+}
+int numeroMayor(int listadoDeNotas[], int tamano){
+    int numeroMayor;
+    for(int i=0;i<tamano;i++){
+        if(i==0 || listadoDeNotas[i]>numeroMayor){
+            numeroMayor= listadoDeNotas[i];
+        }
+    }
+
+    return numeroMayor;
+
+}
+
+int numeroMenor(int listadoDeNotas[], int tamano){
+    int numeroMenor;
+    for(int i=0;i<tamano;i++){
+        if(i==0 || listadoDeNotas[i]<numeroMenor){
+            numeroMenor= listadoDeNotas[i];
+        }
+    }
+
+    return numeroMenor;
+}
+
+int retornarTotal(int listadoDeNotas[], int tamano){
+    int sumaTotal=0;
+    for(int i=0;i<tamano;i++){
+        sumaTotal=sumaTotal+listadoDeNotas[i];
+    }
+    return sumaTotal;
+}
+
+
+
+int retornarPromedio(int listadoDeNotas[],int tamano){
+
+    int suma;
+    int promedio;
+    suma=retornarTotal(listadoDeNotas,tamano);
+    promedio=suma/tamano;
+    return promedio;
+}
+
+
+void cargarListado(int listadoDeNotas[],int tamano){
     int numeroIngresado;
-
-
-
-/*printf("%d",sizeof(numeroIngresado));
-    printf("\n%d",sizeof(listadoDeNotas));
-    printf("\n%d",&numeroIngresado);
-    printf("\n%d",listadoDeNotas);
-
-    for(int i=0;i<5;i++){
-        printf("\n direccion:  %d", &listadoDeNotas[i]);
-
-    }*/
-
-    for(int i=0;i<CANTIDAD;i++){
-
-        printf("\n Ingrese nota: ");
+    for(int i=0;i<tamano;i++){
+        printf("ingrese una nota: ");
         scanf("%d", &numeroIngresado);
 
         listadoDeNotas[i]=numeroIngresado;
 
     }
-    for(int i=0;i<CANTIDAD;i++){
-
-        printf("\n la nota es: %d",listadoDeNotas[i]);
-    }
-
-
-    return 0;
-}
-
-int utn_validarInt(int auxNumero,int min , int max){
-    int retorno=0;
-    if(auxNumero<=min || auxNumero>=max){
-        retorno=-1;
-    }
-    return retorno;
-}
-
-int utn_getNumero(int* pResultado,char* mensaje,char* mensajeError,int minimo,int maximo,int reintentos){
-    int retorno=0;
-    int auxNumero;
-    do{
-        __fpurge(stdin);
-        printf("%s \n",mensaje);
-        if(scanf("%d",&auxNumero)!=1 || utn_validarInt(auxNumero,minimo,maximo)!=0){
-            system("clear");
-            printf("%s\n",mensajeError);
-            reintentos--;
-        }else{
-            *pResultado=auxNumero;
-            system("clear");
-            break;
-        }
-        if(reintentos==0){
-            system("clear");
-            printf("Pasaste el limite de intentos.Te devolveremos al menu para que elegir otra operacion.\n\n");
-            retorno=-1;
-        }
-    }while(reintentos>0);
-    return retorno;
 }
 
