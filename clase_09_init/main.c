e5c7a11d6339cfd908b8e0cf71a23c43cbcb31f4
alumno <alumno@LAB00PC00>
2018-09-18 22:12:44 -0300

    clase

---
+++ clase_09_init/main.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "utn.h"
+#define CANTIDAD 200
+/*
+
+1)primero hacer un struc con atributos nombre , descripcion y  precio
+2)un array de longitud 200 el tipo de la array es producto hacer un printf que nos diga que no esta cargado poner todo en null
+3)una funcion , donde  reciba el array nuestro de productos , indice ,
+
+
+
+
+1)  Definir un tipo de dato que represente un producto.
+    El producto tiene un nombre, una descripcion y un precio
+
+2)  Definir un array de 200 productos, indicar de alguna manera
+    que la info de cada item no esta cargada.
+
+3)  Realizar una funcion que reciba el array, un indice, y le
+    permita al usuario cargar los datos en el item de la posicion especificada por el indice.
+
+4)  Realizar una funcion que reciba el array y un indice e imprima
+    los datos del item de la posicion especificada por el indice.
+
+5)  Realizar una funcion que me devuelva el indice de un item vacio (sin cargar).
+
+
+3) Realizar un programa con un menu de dos opciones:
+    a) Cargar un producto
+    b) Imprimir lista productos
+
+
+4)  Agregar al tipo de dato el campo ID que represente un identificador unico.
+    Modificar el codigo anterior para que el ID se genere automaticamente. Se debera cargar el ID automaticamente al cargar un producto, y se debera imprimir al imprimir la lista.
+
+5)  Realizar una funcion que reciba el array y un ID, y me devuelva el indice
+    del item con dicho ID.
+
+6)  Realizar una funcion que reciba el array, un indice, y le permita al usuario
+    modificar los campos nombre y precio del item del array en la posicion especificada por el indice.
+
+
+7)  Agregar una opcion en el menu. "Editar producto" que pida al usuario el ID del
+    mismo y le permita cambiar el nombre y el precio.
+
+8)  Agregar una opcion en el menu "Borrar producto" que pida al usuario el ID del
+    mismo.
+
+*/
+int main()
+{
+
+    return 0;
+}
+
+
+
+
+
+
